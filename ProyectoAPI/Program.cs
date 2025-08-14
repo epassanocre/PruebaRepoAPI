a28a1c770605579ca8227972773f1f9caf020c46
Eduardo Augusto Passano Chávez <epassanoc@rree.gob.pe>
2025-08-14 08:38:54 -0500

    cambio de prueba

--- ProyectoAPI/Program.cs
+++ ProyectoAPI/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Se agregó comentario de prueba
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
